e27b6b11dbf0457f473a5173f1bb01de3447321a
Footagesus <alisher.bahitbek.discord2@gmail.com>
2025-02-10 14:06:09 +0500

    lag fix

--- Main.lua
+++ Main.lua
@@ -2,12 +2,12 @@
 -- https://raw.githubusercontent.com/Footagesus/Icons/refs/heads/main/lucide/dist/Icons.lua
 
 local Icons = {
-    ["lucide"] = "https://raw.githubusercontent.com/Footagesus/Icons/refs/heads/main/lucide/dist/Icons.lua",
-    ["craft"] = "https://raw.githubusercontent.com/Footagesus/Icons/refs/heads/main/craft/dist/Icons.lua",
+    ["lucide"] = loadstring(game:HttpGetAsync("https://raw.githubusercontent.com/Footagesus/Icons/refs/heads/main/lucide/dist/Icons.lua"))(),
+    ["craft"] = loadstring(game:HttpGetAsync("https://raw.githubusercontent.com/Footagesus/Icons/refs/heads/main/craft/dist/Icons.lua"))(),
     
     
     -- More soon 
-    -- :)
+    -- 
 }
 
 
@@ -20,7 +20,7 @@ function IconModule.SetIconsType(iconType)
 end
 
 function IconModule.Icon(Icon, Type) -- Type: optional
-    local iconType = loadstring(game:HttpGetAsync( Icons[Type or IconModule.IconsType] ))()
+    local iconType = Icons[Type or IconModule.IconsType]
     
     if iconType.Icons[Icon] then
         return { iconType.Spritesheets[tostring(iconType.Icons[Icon].Image)], iconType.Icons[Icon] }
